810e53e7b61d3222f9c1cae4b56293cbbf567eeb
leeyoungchun <dldydcns123@gmail.com>
2022-04-25 22:44:55 +0900

    Auditing

--- src/main/java/com/study/datajpa/DataJpaApplication.java
+++ src/main/java/com/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package com.study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +17,10 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		//스프링 시큐리티 홀더에서 유저정보를 넣어줘야함
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
